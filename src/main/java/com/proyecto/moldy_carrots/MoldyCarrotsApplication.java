56f568f7010c89d4b24c7cda98e7744a16aa3b43
arielGs_Dev <arielogavilanes1534@gmail.com>
2024-10-22 19:19:53 -0500

    first commit

---
+++ src/main/java/com/proyecto/moldy_carrots/MoldyCarrotsApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.moldy_carrots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoldyCarrotsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoldyCarrotsApplication.class, args);
+	}
+
+}
